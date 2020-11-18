f787166011f5957a735fef46bafe9c3d383e1593
uiliam11 <100007488.joan23@fje.edu>
2020-11-18 12:21:23 +0100

    subida 18/11

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-/* Barra de navegación*/
 window.onscroll = function() {
     scrollFunction()
 };
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("nav").style.backgroundColor = "white";
+        document.getElementById("nav").style.backgroundColor = "rgba(0,0,0,0.0)";
     } else {
         document.getElementById("nav").style.backgroundColor = "rgba(0,0,0,0.0)";
     }
